fc40b46c769b50c03368c470411db88d2f8e3c88
Aum2014 <aumfer2011@hotmail.co.th>
2014-12-05 16:55:21 +0700

    SecondCommitAddingDataSubmit

--- WebApplication3/WebForm2.aspx.cs
+++ WebApplication3/WebForm2.aspx.cs
@@ -16,7 +16,7 @@ namespace WebApplication3
             strConnString = "Server=AUMFER-PC\\SQLEXPRESS;Initial Catalog=PalceData;Integrated Security=True";
             strSQL = "INSERT INTO PlaceData (P_type,P_many,P_project,P_road,P_nature,P_announce,P_country,P_area,P_price,P_contact,P_tel,P_person,P_email,P_report) " +
             " VALUES " +
-            " ('" + this.TextBox1.Text + "','" + this.TextBox2.Text + "')";
+            " ('" + this.TextBox1.Text + "','" + this.TextBox2.Text + "','" + this.TextBox3.Text + "','" + this.TextBox4.Text + "','" + this.TextBox5.Text + "','" + this.TextBox6.Text + "','" + this.TextBox7.Text + "','" + this.TextBox8.Text + "','" + this.TextBox10.Text + "','" + this.TextBox9.Text + "','" + this.TextBox12.Text + "','" + this.TextBox11.Text + "','" + this.TextBox14.Text + "','" + this.TextBox13.Text + "')";
 
             objConn.ConnectionString = strConnString;
             objConn.Open();
